3a8553808540427edae7078ff0a26dfa4cdc22a7
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-03-07 14:34:06 +0700

    . (#4)

--- ecommerce/src/routers/Router.jsx
+++ ecommerce/src/routers/Router.jsx
@@ -3,19 +3,23 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from '../pages/Home'
-import Catalog from '../pages/Catalog'
+import Laptop from '../pages/Laptop'
 import Cart from '../pages/Cart'
+import Login from '../pages/Login'
+import Mobile from '../pages/Mobile'
+import Accessory from '../pages/Accessory'
 import Product from '../pages/Product'
-import Singin from '../pages/login.page'
 
 const Routes = () => {
     return (
         <Switch>
             <Route path='/' exact component={Home}/>
             <Route path='/catalog/:slug' component={Product}/>
-            <Route path='/catalog' component={Catalog}/>
-            <Route path='/cart' component={Cart}/>
-            <Route path='/login.page' component={Singin}/>
+            <Route path='/Laptop' component={Laptop}/>
+            <Route path='/Mobile' component={Mobile}/>
+            <Route path='/Accessory' component={Accessory}/>
+            <Route path='/Cart' component={Cart}/>
+            <Route path='/Login' component={Login}/>
         </Switch>
     )
 }
