90e92100d15e10600c88254f2775c05d39ff22ce
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-03-10 09:26:46 +0700

    create Header+footer

--- ecommerce/src/components/Layout.jsx
+++ ecommerce/src/components/Layout.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Header from './Header'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Router from '../routes/Router'
+import Footer from './Footer'
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <BrowserRouter>
+                <div>
+                    <Header/>
+                    <div className="container">
+                        <div className="main">
+                            <Router/>
+                        </div>
+                    </div>
+                    <Footer/>
+                </div>
+    </BrowserRouter>
   )
 }
 
--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-
+import './css/Home.css'
 import Layout from './components/Layout'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
