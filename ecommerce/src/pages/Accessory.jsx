f61c53b0a5457aa3aaccce5e4ccd8b467f379aa2
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-03-11 10:32:30 +0700

    ...

--- ecommerce/src/pages/Accessory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Accessory = () => {
-  return (
-    <div>Accessory</div>
-  )
-}
-
-export default Accessory
--- ecommerce/src/routes/Router.jsx
+++ ecommerce/src/routes/Router.jsx
@@ -4,21 +4,17 @@ import { Route, Routes } from 'react-router-dom'
 
 
 import Home from '../pages/Home'
-import Laptop from '../pages/Laptop'
 import Cart from '../pages/Cart'
 import Login from '../pages/Login'
-import Mobile from '../pages/Mobile'
-import Accessory from '../pages/Accessory'
 import Product from '../pages/Product'
+import Shop from '../pages/Shop'
 
 const Router = () => {
     return (
         <Routes>
             <Route path='/' exact component={Home}/>
             <Route path='/catalog/:slug' component={Product}/>
-            <Route path='/Laptop' component={Laptop}/>
-            <Route path='/Mobile' component={Mobile}/>
-            <Route path='/Accessory' component={Accessory}/>
+            <Route path='/Shop' component={Shop}/>
             <Route path='/Cart' component={Cart}/>
             <Route path='/Login' component={Login}/>
         </Routes>
