db6982382bd6314120680cce25c6e10a9a910879
An Nguyễn <nguyenthanhan140701@gmail.com>
2022-03-16 21:14:46 +0700

    Tạo border

--- ecommerce/src/pages/Login.jsx
+++ ecommerce/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login_compo from '../components/Log/Login.jsx'
+import background from '../css/LogStyle/background.css'
 
 
 const Login = () => {
   return (
     
-    <Login_compo/>
+      <Login_compo/>
   )
 }
 
