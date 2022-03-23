3e39a80d74a60d6ef32ce1ca9a667bc0029ae903
HienNguyen120201 <90005023+HienNguyen120201@users.noreply.github.com>
2022-03-23 15:38:55 +0700

    Update ProductList.jsx

--- ecommerce/src/components/Shop/ProductList.jsx
+++ ecommerce/src/components/Shop/ProductList.jsx
@@ -3,35 +3,33 @@ import ProductItem from "./ProductItem"
 import Grid from "@mui/material/Grid"
 import { useSelector } from "react-redux"
 
-const formatVND = (num) => {
-   return new Intl.NumberFormat("vi-VN", {
-      style: "currency",
-      currency: "VND",
-   }).format(num)
-}
-
-function ProductList() {
+function ProductList({cate}) {
    const products = useSelector((state) => state.shop.products)
+   const filteredProducts =  useSelector((state) => state.shop.filterProducts)
+   const isFilter =  useSelector((state) => state.shop.isFilter)
+   console.log(products)
+   let list = isFilter ? filteredProducts: products.filter(product => product.type === cate)
+   console.log("hehe")
    return (
       <>
          <Grid container style={{ marginTop: "5rem" }}>
-            {products.map((item, idx) => {
-               const sale = item.market_price - item.discount_price
-               const sale_text = item.market_price > item.discount_price ? `Giảm ${formatVND(sale)}` : null
+            {list.map((item, idx) => {
+               // const sale = item.market_price - item.discount_price
+               // const sale_text = item.market_price > item.discount_price ? `Giảm ${formatVND(sale)}` : null
                return (
                   <Grid item lg={4} key={idx}>
                      <ProductItem
-                        id={item.id}
+                        id={item.productId}
                         title={item.name}
-                        imgSrc={item.imageList}
-                        discount_price={item.discount_price}
-                        market_price={item.market_price}
+                        imgSrc={[item.imgUrl1, item.imgUrl2, item.imgUrl3]}
+                        discount_price={item.unitPrice}
+                        market_price={item.oldPrice}
                         isSale={item.market_price > item.discount_price ? true : false}
-                        saleText={sale_text}
-                        short_description={item.short_description}
-                        rating_average={item.rating_average}
-                        review_count={item.review_count}
-                        sale_count={item.sale_count}
+                        saleText={item.status}
+                        short_description={item.description}
+                        rating_average={item.rating}
+                        review_count={200}
+                        sale_count={500}
                      />
                   </Grid>
                )
